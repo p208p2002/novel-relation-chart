4342c1afbc43a01248d81d18b61a496559201a2b
Philip <p208p2002@gmail.com>
2020-03-20 10:00:30 +0800

    no message

--- site/src/App.js
+++ site/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ChartView from './modules/chartModules'
-
+import './App.css'
 export class App extends Component {
   constructor(props) {
     super(props)
@@ -8,16 +8,21 @@ export class App extends Component {
   }
   render() {
     return (
-      <div className="container">
-        <br/>
-        <div className="text-center">
-          <img 
-            style={{maxWidth:'550px',width:'100%'}}
-            src={require("./assets/imgs/logo.png")} 
-            alt="絕代雙驕人物關係圖"
-            srcset="" />
+      <div>
+        <div className="container">
+          <div className="d-none d-sm-block">
+            <a class="github-fork-ribbon" href="https://github.com/p208p2002/novel-relation-chart" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
+          </div>
           <br />
-          <br />
+          <div className="text-center" style={{width:'100%'}}>
+            <img
+              style={{ maxWidth: '550px', width: '100%' }}
+              src={require("./assets/imgs/logo.png")}
+              alt="絕代雙驕人物關係圖"
+              srcset="" />
+            <br />
+            <br />
+          </div>
         </div>
         <ChartView width="100%" height="500px" />
       </div>
